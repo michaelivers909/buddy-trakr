12ed7f4c873e5d15f8159160487feb4988a9ef6e
Mike Truax <mike@miketruax.com>
2021-03-11 13:21:15 -0600

    builds axios hook for login

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,15 +1,50 @@
-import { useState, useEffect, useMemo, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
-export const useAxios = (url) => {
+/**
+ *
+ * @param {String} errorKey Key where any potential errors might be returned from the server
+ * @returns
+ */
+export const useAxios = (errorKey = null) => {
   const isMountedRef = useRef(false);
-
-  // Inside the hooks effect, change the isMountedRef's current key to true. Remember, useEffect only runs when the component is rendered
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
   useEffect(() => {
     isMountedRef.current = true;
-    if (isMountedRef.current) setSomeStateKey("NEW VALUE");
     return () => {
       isMountedRef.current = false;
     };
-  });
+  }, []);
+  /**
+   *
+   * @param {String} url Url of the request
+   * @param {String} type Type of HTTP Request to make
+   * @param {object} [config] Configuration for the request as per the Axios Docs
+   */
+
+  async function apiCall(url, type, config) {
+    try {
+      setLoading(true);
+      const res = axios({ url, type, ...config });
+
+      if (!isMountedRef.current) {
+        return;
+      }
+      setLoading(false);
+      if (errorKey && res.data[errorKey]) {
+        return setError(res.data[errorKey]);
+      }
+      setData(res.data);
+    } catch (err) {
+      if (!isMountedRef.current) {
+        return;
+      }
+      setLoading(false);
+      setError("Something went wrong, please try again later.");
+    }
+  }
+
+  return { error, loading, data, apiCall };
 };
